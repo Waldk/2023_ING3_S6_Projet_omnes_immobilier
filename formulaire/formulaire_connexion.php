50ec799f8463e956db994d908d8812031a2db00c
Elliot <elliothoms4@gmail.com>
2023-03-03 18:39:24 +0100

    ajout css pour la connexion à un compte

--- formulaire/formulaire_connexion.php
+++ formulaire/formulaire_connexion.php
@@ -1,27 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Connexion à votre compte</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="../prime.css" rel="stylesheet" type="text/css" />
+	<link href="../connexion.css" rel="stylesheet" type="text/css"/>
     <link rel="shortcut icon" href="../image/OMNES.ico">
-	<title>Verification mot de passe et login</title>
+    <script src="https://kit.fontawesome.com/92cddb0a79.js" crossorigin="anonymous"></script>
 </head>
 <body>
-	<form action="../scripts/connexion.php" method="post">
-		<table border="2">
-			<tr>
-				<td>Identifiant</td>
-				<td><input type="text" name="identifiant"></td>
-			</tr>
-			<tr>
-				<td>Mot de passe</td>
-				<td><input type="password" name="mot_de_passe"></td>
-			</tr>
-			<tr>
-				<td colspan="2" align="center"><input type="submit" name="submit" value="valider"></td>
-			</tr>
-		</table>
-	</form>
+	<div id="wrapper">
+        <div id="container">
+			<form action="../scripts/connexion.php" method="post">
+
+				<div class="logo">
+					<img src="../image/OMNES.ico" style="width: 40%; height: auto;">
+					<h1>Connexion</h1>
+				</div>
+				
+				<br><hr>
+
+				<label><b>Nom d'utilisateur</b></label>
+				<input type="text" placeholder="Entrer le nom d'utilisateur" name="identifiant" required>
+
+				<label><b>Mot de passe</b></label>
+				<input type="password" placeholder="Entrer le mot de passe" name="mot_de_passe" required>
+
+				<input type="submit" id='submit' value='valider' >
+				<?php
+				if(isset($_POST['erreur'])){
+				$err = $_POST['erreur'];
+				if($err==1 || $err==2)
+				echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+				}
+				?>
+			</form>
+ 		</div>
+    </div>
 </body>
 </html>
