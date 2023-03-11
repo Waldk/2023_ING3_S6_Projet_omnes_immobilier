b27620d17811d16066de905a29cc8a07028e5ec8
Elliot <elliothoms4@gmail.com>
2023-03-11 18:41:54 +0100

    ajout lien ajax

--- compte.php
+++ compte.php
@@ -10,6 +10,7 @@
     <link rel="shortcut icon" href="image/OMNES.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/92cddb0a79.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             $('#button_modif').click(function () {
