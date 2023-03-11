fbfe030c5d6076a515825aef4711250057f2bb9d
Elliot <elliothoms4@gmail.com>
2023-03-11 18:52:22 +0100

    Modification page admin

--- scripts/compte_admin.php
+++ scripts/compte_admin.php
@@ -1,8 +1,7 @@
 <?php
-    include("scripts/affichage_info.php");
     echo'
-    <center><button class="bouton_footer" id="button_bien">Cliquez pour ajouter un bien</button>
-    <button class="bouton_footer" id="button_users">Cliquez pour supprimer un compte</button></center>
+    <center><button class="bouton_footer" id="button_bien">Ajouter un bien</button>
+    <button class="bouton_footer" id="button_users">Aupprimer un compte</button></center>
     ';
     include("formulaire/formulaire_ajout_bien.php");
     include("scripts/affichage_delete_user.php");
