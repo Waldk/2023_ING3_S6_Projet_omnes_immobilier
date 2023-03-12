f22b91b6a2e0c584c23ccd917d6a95f78b80cefc
Waldk <99488859+Waldk@users.noreply.github.com>
2023-03-12 21:53:26 +0100

    ca avance bien bien

--- scripts/compte_agent.php
+++ scripts/compte_agent.php
@@ -1,7 +1,6 @@
 <?php
     include("scripts/affichage_info.php");
     include("scripts/modifier_info.php");
-    include("scripts/affichage_calendrier_semaine.php");
     include("scripts/script_affichage_rdv_agent.php");
     include("scripts/script_affichage_rdv_passe_agent.php");
 ?>
