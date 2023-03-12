3bd52780e52ed2be8fb7010060df0c170813e392
Waldk <99488859+Waldk@users.noreply.github.com>
2023-03-12 23:10:13 +0100

    visuel nom

--- scripts/script_affichage_bien.php
+++ scripts/script_affichage_bien.php
@@ -20,11 +20,11 @@ while ($row = mysqli_fetch_assoc($resultat)) {
     echo
         "<li class=\"element-carousel\">
                     <div class=\"card\">
-                        <h2 class=\"card-title\">$nom</h2>
+                        <h2 class=\"card-title\">".utf8_encode($nom)."</h2>
                         <img style=\"width :700px; height : 300px;\" src=\"data:image/jpg;base64," . base64_encode($photo) . "\" />
                         <div class=\"card-content\">
-                            <p>$description</p>
-                            <a href=\"Detail_batiment.php?id_bien=$id_bien\" class=\"button\">Read more</a>
+                            <p>".utf8_encode($description)."</p>
+                            <a href=\"Detail_batiment.php?id_bien=$id_bien\" class=\"button\">Voir plus</a>
                         </div>
                     </div>
                 </li>";
