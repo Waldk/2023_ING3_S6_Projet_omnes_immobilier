e00c0c964db511a7a28f1612f3b352e801effbfa
Waldk <99488859+Waldk@users.noreply.github.com>
2023-03-12 22:12:15 +0100

    correction deconnexion

--- testcalendrier.php
+++ testcalendrier.php
@@ -8,28 +8,46 @@
 
 <body>
     <?php
-    $user = $_SESSION['Omnes']['user_id'];
-    $mysqli = new mysqli("localhost", "root", "", "test_projet_1");
+    if (isset($_SESSION['Omnes']['user_id'])) {
+        $user = $_SESSION['Omnes']['user_id'];
+        $mysqli = new mysqli("localhost", "root", "", "test_projet_1");
 
-    if ($mysqli->connect_error) {
-        die("Erreur de connexion à la base de données: " . $mysqli->connect_error);
-    }
-
-    $booked_dates = array();
-    $result = $mysqli->query("SELECT date FROM rdv WHERE Agent_immo = '" . $identifiant_agent . "'");
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $booked_dates[] = $row["date"];
+        if ($mysqli->connect_error) {
+            die("Erreur de connexion à la base de données: " . $mysqli->connect_error);
         }
+
+        $booked_dates = array();
+        $result = $mysqli->query("SELECT date FROM rdv WHERE Agent_immo = '" . $identifiant_agent . "'");
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $booked_dates[] = $row["date"];
+            }
+        }
+
+        $booked_dates_user = array();
+        $result2 = $mysqli->query("SELECT date FROM rdv WHERE Client = '" . $user . "'");
+        if ($result2->num_rows > 0) {
+            while ($row = $result2->fetch_assoc()) {
+                $booked_dates_user[] = $row["date"];
+            }
+        }
+    } else {
+        $mysqli = new mysqli("localhost", "root", "", "test_projet_1");
+
+        if ($mysqli->connect_error) {
+            die("Erreur de connexion à la base de données: " . $mysqli->connect_error);
+        }
+
+        $booked_dates = array();
+        $result = $mysqli->query("SELECT date FROM rdv WHERE Agent_immo = '" . $identifiant_agent . "'");
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $booked_dates[] = $row["date"];
+            }
+        }
+        $booked_dates_user=$booked_dates;
     }
 
-    $booked_dates_user = array();
-    $result2 = $mysqli->query("SELECT date FROM rdv WHERE Client = '" . $user . "'");
-    if ($result2->num_rows > 0) {
-        while ($row = $result2->fetch_assoc()) {
-            $booked_dates_user[] = $row["date"];
-        }
-    }
     ?>
 
     <table class="calendrier">
@@ -44,23 +62,29 @@
             <th>Dimanche</th>
         </tr>
 
-        <?php for ($i = 8; $i < 20; $i++){
+        <?php for ($i = 8; $i < 20; $i++) {
             echo "<tr>
                 <td>";
-                echo sprintf("%02d", $i) . "h-" . sprintf("%02d", $i + 1) . "h";
-                echo "</td>";
-                for ($j = 0; $j < 7; $j++){
-                     $date = date("Y-m-d H:i:s", strtotime("next Monday") + ($j * 24 * 3600) + ($i * 3600));
-                     if (in_array($date, $booked_dates_user)){
-                        echo "<td class=\"booked_user\"></td>";
-                     } else if (in_array($date, $booked_dates)){
+            echo sprintf("%02d", $i) . "h-" . sprintf("%02d", $i + 1) . "h";
+            echo "</td>";
+            for ($j = 0; $j < 7; $j++) {
+                $date = date("Y-m-d H:i:s", strtotime("next Monday") + ($j * 24 * 3600) + ($i * 3600));
+                if (in_array($date, $booked_dates_user)) {
+                    if (!isset($_SESSION['Omnes']['user_id'])){
                         echo "<td class=\"booked\"></td>";
-                     } else {
-                        echo "<td style=\"color : white;\"><a href=\"prise_rdv.php?date=".$date."&identifiant_agent=". $identifiant_agent."&nom_lieu=". $nom_lieu."&id_bien=". $id_bien."\">rdvaaaaaaaaaaaaaaa</a></td>";
                     }
-                } 
-            echo"</tr>";
-        }?>
+                    else {
+                        echo "<td class=\"booked_user\"></td>";
+                    }
+                    
+                } else if (in_array($date, $booked_dates)) {
+                    echo "<td class=\"booked\"></td>";
+                } else {
+                    echo "<td style=\"color : white;\"><a href=\"prise_rdv.php?date=" . $date . "&identifiant_agent=" . $identifiant_agent . "&nom_lieu=" . $nom_lieu . "&id_bien=" . $id_bien . "\">rdvaaaaaaaaaaaaaaa</a></td>";
+                }
+            }
+            echo "</tr>";
+        } ?>
     </table>
 
     <?php
